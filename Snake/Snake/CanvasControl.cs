4a9c5afd77c44efa5980e3c6df20dfcde31ba8f5
Mislav Žanić <zanicmis.math@pmf.hr>
2022-01-27 08:37:03 -0800

    strelice radi

--- Snake/Snake/CanvasControl.cs
+++ Snake/Snake/CanvasControl.cs
@@ -21,5 +21,30 @@ namespace Snake
                 true);
             InitializeComponent();
         }
+
+        protected override bool IsInputKey(Keys keyData)
+        {
+            if (keyData == Keys.Up)
+            {
+                return true;
+            }
+            else if (keyData == Keys.Down)
+            {
+                return true;
+            }
+            else if (keyData == Keys.Left)
+            {
+                return true;
+            }
+            else if (keyData == Keys.Right)
+            {
+                return true;
+            }
+            else
+            {
+                return base.IsInputKey(keyData);
+            }
+        }
+
     }
 }
