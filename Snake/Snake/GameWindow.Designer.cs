03b084b49ef39d25b52214e347ff160e687419d8
enaskopelja <ena.skopelja@gmail.com>
2022-01-29 23:27:39 +0100

    ikona i boje pozadine i gumbica

--- Snake/Snake/GameWindow.Designer.cs
+++ Snake/Snake/GameWindow.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameWindow));
             this.canvasControl1 = new Snake.CanvasControl();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -36,13 +37,25 @@
             this.scoreLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
+            // scoreLabel
+            // 
+            this.scoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Gadugi", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(1032, 768);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(68, 25);
+            this.scoreLabel.TabIndex = 4;
+            this.scoreLabel.Text = "Score:";
+            // 
             // canvasControl1
             // 
-            this.canvasControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.canvasControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.canvasControl1.Location = new System.Drawing.Point(0, 0);
-            this.canvasControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.canvasControl1.Margin = new System.Windows.Forms.Padding(6);
             this.canvasControl1.Name = "canvasControl1";
             this.canvasControl1.Size = new System.Drawing.Size(919, 865);
             this.canvasControl1.TabIndex = 0;
@@ -53,55 +66,52 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Gadugi", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Linen;
             this.button1.Location = new System.Drawing.Point(1037, 34);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(268, 106);
             this.button1.TabIndex = 1;
             this.button1.Text = "New Game";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.NewGame);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(1037, 177);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.BackColor = System.Drawing.Color.Olive;
+            this.button2.Font = new System.Drawing.Font("Gadugi", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(1037, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(268, 98);
             this.button2.TabIndex = 2;
             this.button2.Text = "Settings";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.SettingsPreview);
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.tick);
             // 
-            // scoreLabel
-            // 
-            this.scoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.scoreLabel.AutoSize = true;
-            this.scoreLabel.Location = new System.Drawing.Point(1059, 327);
-            this.scoreLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(74, 25);
-            this.scoreLabel.TabIndex = 4;
-            this.scoreLabel.Text = "Score:";
-            // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Linen;
             this.ClientSize = new System.Drawing.Size(1409, 865);
             this.Controls.Add(this.scoreLabel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.canvasControl1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "GameWindow";
-            this.Text = "GameWindow";
+            this.Text = "Snake";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyPressed);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -115,5 +125,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label scoreLabel;
+
     }
 }
